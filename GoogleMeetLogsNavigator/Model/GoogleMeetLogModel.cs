5d40e55683c2bea7f160a002ff5d54badb3c7755
Galis97 <ugalis97@gmail.com>
2020-12-24 16:25:26 +0100

    add model and constants

--- GoogleMeetLogsNavigator/Model/GoogleMeetLogModel.cs
+++ GoogleMeetLogsNavigator/Model/GoogleMeetLogModel.cs
@@ -4,13 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
 namespace GoogleMeetLogsNavigator.Model
 {
+   
     class GoogleMeetLogModel
     {
         #region Mandatory Data
 
-        public string Date { get; set; }
+        public DateTime Date { get; set; }
         public string EventName { get; set; }
         public string EventDescription { get; set; }
         public string MeetingCode { get; set; }
@@ -21,7 +24,7 @@ namespace GoogleMeetLogsNavigator.Model
     
         public string ProductType { get; set; }
     
-        public string Duration { get; set; }
+        public int Duration { get; set; }
     
         public string CallEvaluationOn5 { get; set; }
     
@@ -51,90 +54,48 @@ namespace GoogleMeetLogsNavigator.Model
 
         #region Optional Data
 
-    
         public string NETRoundTrip { get; set; }
-    
+
         public string TransportProtocol { get; set; }
-    
-        public string PredictedBandWidthLoading { get; set; }
-    
-        public string PredictedBandWidthUploading { get; set; }
-
-    
-        public string MaxReceptionAudioPacketsLost { get; set; }
-    
-        public string AverageReceptionAudioPacketsLost { get; set; }
-    
-        public string AudioReceptionDuration { get; set; }
-    
-        public string BitRationAudioSending { get; set; }
-    
-        public string MaxSendingAudioPacketsLost { get; set; }
-        
-        public string AverageSendingAudioPacketsLost { get; set; }
-        
-        public string AudioSendingDuration { get; set; }
-        
-        public string AverageReceptionFlickering { get; set; }
-        
-        public string MaxReceptionFilckering { get; set; }
-        
-        public string AverageSendingFlickering { get; set; }
-        
-        public string BitRationScreencastReception { get; set; }
-        
-        public string AverageScreecastReception { get; set; }
-        
-        public string LongSideMedianScreencastReception { get; set; }
-        
-        public string MaxReceptionScreencastPacketsLost { get; set; }
-        
-        public string AverageReceptionScreencastPacketsLost { get; set; }
-        
-        public string ScreencastReceptionDuration { get; set; }
-        
-        public string ShortSideMedianScreencastReception { get; set; }
-        
-        public string BitRationScreencastSending { get; set; }
-        
-        public string AverageScreecastSending { get; set; }
-
-        public string LongSideMedianScreencastSending { get; set; }
-
-        public string MaxSendingScreencastPacketsLost { get; set; }
-
-        public string AverageSendingScreencastPacketsLost { get; set; }
-
-        public string ScreencastSendingDuration { get; set; }
-
-        public string ShortSideMedianScreencastSending { get; set; }
-
-        public string AverageVideoReception { get; set; }
-
-        public string LongSideMedianVideoReception { get; set; }
-
-        public string MaxVideoReceptionPacketsLost { get; set; }
-
-        public string AverageVideoReceptionPacketsLost { get; set; }
-
-        public string ReceptionVideoDuration { get; set; }
-
-        public string ShortSideMedianVideoReception { get; set; }
-
-        public string BitRationVideoSending { get; set; }
-
-        public string AverageVideoSending { get; set; }
-
-        public string LongSideMedianVideoSending { get; set; }
-
-        public string MaxSendingVideoPacketsLost { get; set; }
-
-        public string AverageSendingVideoPacketsLost { get; set; }
-
-        public string VideoSenfingDuration { get; set; }
-
-        public string ShortSideMedianVideoSending { get; set; }
-
+        public int PredictedBandWidthLoading { get; set; }
+        public int PredictedBandWidthUploading { get; set; }
+        public int MaxReceptionAudioPacketsLost { get; set; }
+        public int AverageReceptionAudioPacketsLost { get; set; }
+        public int AudioReceptionDuration { get; set; }
+        public int BitRationAudioSending { get; set; }
+        public int MaxSendingAudioPacketsLost { get; set; }
+        public int AverageSendingAudioPacketsLost { get; set; }
+        public int AudioSendingDuration { get; set; }
+        public int AverageReceptionFlickering { get; set; }
+        public int MaxReceptionFilckering { get; set; }
+        public int AverageSendingFlickering { get; set; }
+        public int BitRationScreencastReception { get; set; }
+        public int AverageScreecastReception { get; set; }
+        public int LongSideMedianScreencastReception { get; set; }
+        public int MaxReceptionScreencastPacketsLost { get; set; }
+        public int AverageReceptionScreencastPacketsLost { get; set; }
+        public int ScreencastReceptionDuration { get; set; }
+        public int ShortSideMedianScreencastReception { get; set; }
+        public int BitRationScreencastSending { get; set; }
+        public int AverageScreecastSending { get; set; }
+        public int LongSideMedianScreencastSending { get; set; }
+        public int MaxSendingScreencastPacketsLost { get; set; }
+        public int AverageSendingScreencastPacketsLost { get; set; }
+        public int ScreencastSendingDuration { get; set; }
+        public int ShortSideMedianScreencastSending { get; set; }
+        public int AverageVideoReception { get; set; }
+        public int LongSideMedianVideoReception { get; set; }
+        public int MaxVideoReceptionPacketsLost { get; set; }
+        public int AverageVideoReceptionPacketsLost { get; set; }
+        public int ReceptionVideoDuration { get; set; }
+        public int ShortSideMedianVideoReception { get; set; }
+        public int BitRationVideoSending { get; set; }
+        public int AverageVideoSending { get; set; }
+        public int LongSideMedianVideoSending { get; set; }
+        public int MaxSendingVideoPacketsLost { get; set; }
+        public int AverageSendingVideoPacketsLost { get; set; }
+        public int VideoSenfingDuration { get; set; }
+        public int ShortSideMedianVideoSending { get; set; }
         public string NetworkCongestion { get; set; }
 
         //TO ADD IN specular of this object model
@@ -145,6 +106,10 @@ namespace GoogleMeetLogsNavigator.Model
          Se l'id del partecipante non è presente riferirsi a nome del partecipante che ce l'ho sempre a 
          disposizione mentre  l'indirizzo IP in anonimo non l'avrò mai
          */
+        public DateTime MeetingStartData { get; set; }
+        public DateTime MeetingEndData { get; set; }
+        public DateTime MeetingEnteringData { get; set; }
+        public int TotalMeetingUserPartecipation { get; set; }
 
         #endregion
     }
---
+++ GoogleMeetLogsNavigator/Utility/Constants.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleMeetLogsNavigator
+{
+    /// <summary>
+    /// Constants
+    /// </summary>
+    public class Constants
+    {
+        /// <summary>
+        /// Google Meet Csv File HEADER
+        /// </summary>
+        public class CSVHeader
+        {
+            #region Mandatory Data
+
+            public string Date = "Data";
+            public string EventName = "Nome evento";
+            public string EventDescription = "Descrizione evento";
+            public string MeetingCode = "Codice riunione";
+            public string PartecipantIdentifier = "Identificatore partecipante";
+            public string ExternalPartecipantIdentifier = "Partecipante esterno all'organizzazione";
+            public string ClientType = "Tipo di client";
+            public string MeetingOwnerEmail = "Email organizzatore";
+            public string ProductType = "Tipo di prodotto";
+            public string Duration = "Durata";
+            public string CallEvaluationOn5 = "Valutazione chiamata su 5";
+            public string PartecipantName = "Nome partecipante";
+            public string IPAddress = "Indirizzo IP";
+            public string City = "CittÃ";
+            public string Nation = "Paese";
+            public string ActionCause = "Motivo azione";
+            public string ActionDescription = "Descrizione azione";
+            public string VisualizedDestinationName = "Nomi visualizzati di destinazione";
+            public string DetinationEmailsAddresses = "Email di destinazione";
+            public string DestinationPhoneNumber = "Numero telefono di destinazione";
+            public string CalendarEventIdentifier = "ID evento Calendar";
+            public string ConferenceID = "ID conferenza";
+
+            #endregion Mandatory Data
+
+            #region Optional Data
+
+            public string NETRoundTrip = "Media tempo di round trip rete in ms";
+            public string TransportProtocol = "Protocollo di trasporto";
+            public string PredictedBandWidthLoading = "Larghezza di banda per il caricamento in kbps prevista";
+            public string PredictedBandWidthUploading = "Larghezza di banda per il download in kbps prevista";
+
+            public string MaxReceptionAudioPacketsLost = "max pacchetti ricezione audio";
+            public string AverageReceptionAudioPacketsLost = "Perdita media pacchetti ricezione audio";
+            public string AudioReceptionDuration = "Durata ricezione audio";
+            public string BitRationAudioSending = "Media velocitÃ  in bit invio audio in kbps";
+            public string MaxSendingAudioPacketsLost = "Perdita max pacchetti invio audio";
+            public string AverageSendingAudioPacketsLost = "Perdita media pacchetti invio audio";
+            public string AudioSendingDuration = "Durata invio audio";
+
+            public string AverageReceptionFlickering = "Media tremolio ricezione rete in ms";
+            public string MaxReceptionFilckering = "Massimo tremolio ricezione rete in ms";
+            public string AverageSendingFlickering = "Media tremolio invio rete in ms";
+
+            public string BitRationScreencastReception = "Media velocitÃ  in bit ricezione screencast in kbps";
+            public string AverageScreecastReception = "Media f/s ricezione screencast";
+            public string LongSideMedianScreencastReception = "Mediana lato lungo ricezione screencast";
+            public string MaxReceptionScreencastPacketsLost = "Perdita max pacchetti ricezione screencast";
+            public string AverageReceptionScreencastPacketsLost = "Perdita media pacchetti ricezione screencast";
+            public string ScreencastReceptionDuration = "Durata ricezione screencast";
+            public string ShortSideMedianScreencastReception = "Mediana lato corto ricezione screencast";
+            public string BitRationScreencastSending = "Media velocitÃ  in bit invio screencast in kbps";
+            public string AverageScreecastSending = "Media f/s invio screencast";
+            public string LongSideMedianScreencastSending = "Mediana lato lungo invio screencast";
+            public string MaxSendingScreencastPacketsLost = "Perdita max pacchetti invio screencast";
+            public string AverageSendingScreencastPacketsLost = "Perdita media pacchetti invio screencast";
+            public string ScreencastSendingDuration = "Durata invio screencast";
+            public string ShortSideMedianScreencastSending = "Mediana lato corto invio screencast";
+
+            public string AverageVideoReception = "Media f/s ricezione video";
+            public string LongSideMedianVideoReception = "Mediana lato lungo ricezione video";
+            public string MaxVideoReceptionPacketsLost = "Perdita max pacchetti ricezione video";
+            public string AverageVideoReceptionPacketsLost = "Perdita media pacchetti ricezione video";
+            public string ReceptionVideoDuration = "Durata ricezione video";
+            public string ShortSideMedianVideoReception = "Mediana lato corto ricezione video";
+            public string BitRationVideoSending = "Media velocitÃ  in bit invio video in kbps";
+            public string AverageVideoSending = "Media f/s invio video";
+            public string LongSideMedianVideoSending = "Mediana lato lungo invio video";
+            public string MaxSendingVideoPacketsLost = "Perdita max pacchetti invio video";
+            public string AverageSendingVideoPacketsLost = "Perdita media pacchetti invio video";
+            public string VideoSenfingDuration = "Durata invio video";
+            public string ShortSideMedianVideoSending = "Mediana lato corto invio video";
+
+            public string NetworkCongestion = "Rapporto congestione della rete";
+
+            public string MeetingStartData = "Data inizio riunione";
+            public string MeetingEndData = "Data fine riunione";
+            public string MeetingEnteringData = "Data ingresso riunione";
+            public string TotalMeetingUserPartecipation = "Durata totale di partecipazione alla riunione";
+
+            #endregion
+
+        }
+    }
+}
