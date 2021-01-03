585cc222e273f2c25bed5539dcca7337eaef49c4
Fedele Simone De Feo <fedelesimone.defeo@gmail.com>
2021-01-03 17:04:00 +0100

    Setted table readonly

--- GoogleMeetLogsNavigator/Form3.Designer.cs
+++ GoogleMeetLogsNavigator/Form3.Designer.cs
@@ -163,6 +163,7 @@ namespace GoogleMeetLogsNavigator
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(421, 71);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.Size = new System.Drawing.Size(368, 367);
             this.dataGridView1.TabIndex = 5;
             this.dataGridView1.Visible = false;
